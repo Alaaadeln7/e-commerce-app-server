80fab3fd2287fad2f87e7cb4722660da28ced7f5
alaadeln7 <alaadeln7@gmail.com>
2025-04-21 14:02:45 +0200

    change cors options

--- config/constants.js
+++ config/constants.js
@@ -25,7 +25,10 @@ export const COOKIE_OPTIONS = {
 };
 
 export const CORS_OPTIONS = {
-  origin: "http://localhost:5173",
+  origin:
+    process.env.NODE_ENV !== "development"
+      ? "https://gymboyui.onrender.com"
+      : "http://localhost:5173",
   credentials: true,
 };
 
